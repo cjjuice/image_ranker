85d610e99b4fe56f3efa11895d3e91a51822f906
C.J. Bordeleau <cbordeleau@cjs-macbook-pro.(none)>
2012-09-22 00:30:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageRanker::Application.config.secret_token = '4ff7cc9fd4851cc66bdbcc711648cdb54534350ba52ac127b2d4040bb547a4d9613fa3bb0504c3d0dde19d961771fb600f3a33ba6a03a10303e60b21cba60fc2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ImageRanker::Application.config.session_store :cookie_store, key: '_image_ranker_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ImageRanker::Application.config.session_store :active_record_store
