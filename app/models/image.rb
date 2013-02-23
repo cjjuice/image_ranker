de9a2925ef5060e9e0028693166dc6317329ec24
C.J. Bordeleau <bordeleau.cj@gmail.com>
2013-02-23 16:54:28 -0500

    Tweek ranking algo.

--- app/models/image.rb
+++ app/models/image.rb
@@ -9,11 +9,11 @@ class Image < ActiveRecord::Base
   validates_presence_of :image_url , :message => "Must submit an image!"
   validates_format_of   :image_url , :with => %r{\.(jpg|gif|png)$}i , :message => "Link must be an image!"
   
-  scope :ordered_by_score, lambda { order_by_score }
+  default_scope lambda { order_by_score }
 
   def self.order_by_score
     age_in_hours = "(#{Time.now.tv_sec} - EXTRACT (EPOCH FROM images.created_at))/3600"
-    total_score = "(rs_reputations.value - 1)/((#{age_in_hours}) + 2)^1.5"
+    total_score = "rs_reputations.value/((#{age_in_hours}) + 2)^1.5 DESC"
 
     joins("INNER JOIN rs_reputations ON rs_reputations.target_id = images.id").where("rs_reputations.target_type = 'Image'").order(total_score)
   end
