519b6f159ac80daf5842561036b90a3f149bca7b
C.J. Bordeleau <bordeleau.cj@gmail.com>
2013-02-23 16:54:28 -0500

    Tweek ranking algo.

--- app/models/image.rb
+++ app/models/image.rb
@@ -13,7 +13,7 @@ class Image < ActiveRecord::Base
 
   def self.order_by_score
     age_in_hours = "(#{Time.now.tv_sec} - EXTRACT (EPOCH FROM images.created_at))/3600"
-    total_score = "(rs_reputations.value - 1)/((#{age_in_hours}) + 2)^1.5"
+    total_score = "rs_reputations.value/((#{age_in_hours}) + 2)^1.5 DESC"
 
     joins("INNER JOIN rs_reputations ON rs_reputations.target_id = images.id").where("rs_reputations.target_type = 'Image'").order(total_score)
   end
