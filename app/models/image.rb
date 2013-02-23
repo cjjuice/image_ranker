5afa7dd1f17b4d50521c8b42fafd2abc3133695d
C.J. Bordeleau <bordeleau.cj@gmail.com>
2013-02-23 15:43:18 -0500

    Fix default_scope bug.

--- app/models/image.rb
+++ app/models/image.rb
@@ -9,7 +9,7 @@ class Image < ActiveRecord::Base
   validates_presence_of :image_url , :message => "Must submit an image!"
   validates_format_of   :image_url , :with => %r{\.(jpg|gif|png)$}i , :message => "Link must be an image!"
   
-  default_scope lambda { order_by_score }
+  scope :ordered_by_score, lambda { order_by_score }
 
   def self.order_by_score
     age_in_hours = "(#{Time.now.tv_sec} - EXTRACT (EPOCH FROM images.created_at))/3600"
