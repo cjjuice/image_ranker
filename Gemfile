40c92aee224d99dc7c5e03f44a4cdde5776fd07d
C.J. Bordeleau <bordeleau.cj@gmail.com>
2013-02-26 21:56:21 -0500

    Add client side validations, styling, uri email validation

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem "therubyracer"
 gem "less-rails" #Sprockets (what Rails 3.1 uses for its asset pipeline) supports LESS
 gem "twitter-bootstrap-rails"
 gem 'pg'
+gem 'mail'
+gem 'valid_email'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,13 @@ class User < ActiveRecord::Base
   attr_accessible :username, :email, :password, :password_confirmation
   has_secure_password 
   
+  uri_email_regex = /\A[\w+\-.]+@my\.uri\.edu\z/i
+  
   validates_length_of :username, :maximum => 15
   validates_uniqueness_of :email
   validates_uniqueness_of :username  
   validates_presence_of :username, :email, :password, :password_confirmation
+  validates :email, :email => { :message => "Must be a valid email." } , :format => {:with => uri_email_regex , :message => "must be a valid URI email ( my.uri.edu )"}
   
   has_many :images
   
