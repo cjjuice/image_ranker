eb9f2a7936f06423ef25ae4576d37cd38cc3b772
C.J. Bordeleau <bordeleau.cj@gmail.com>
2013-03-12 22:43:17 -0400

    Fix session bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password]) && user.confirmed? == true
+    if user && user.authenticate(params[:password]) && user.confirmed?
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     elsif user.confirmed? == false
