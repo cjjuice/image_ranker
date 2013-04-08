de4d5235885943a9cd72f629bc238430590a9f46
C.J. Bordeleau <bordeleau.cj@gmail.com>
2013-04-08 13:02:01 -0400

    Remove gif support.

--- app/models/image.rb
+++ app/models/image.rb
@@ -7,7 +7,7 @@ class Image < ActiveRecord::Base
   has_reputation :votes, source: :user, aggregated_by: :sum
   
   validates_presence_of :image_url , :message => "Must submit an image!"
-  validates_format_of   :image_url , :with => %r{\.(jpg|gif|png)$}i , :message => "Link must be an image!"
+  validates_format_of   :image_url , :with => %r{\.(jpg|png)$}i , :message => "Link must be an image!"
   has_many :comments, as: :commentable
   
   self.per_page = 16
